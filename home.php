a6b2d62c5f8fb4664d71d1b9f9239ee0c3dbf0c2
EthemDurguti <durgutiethem@gmail.com>
2022-02-09 09:46:24 +0100

    Update home.php

--- home.php
+++ home.php
@@ -11,6 +11,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Crud Task</title>
+    <style>
+        
+    </style>
 </head>
 
 <body>
